7792defbd0e394cde2c0433a58b3ee16641e48e9
Zero0242 <pedrocorream66@gmail.com>
2024-04-07 21:39:53 -0400

    feat (cron): contador de cron

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('simple app');
+const cron = require('node-cron');
+
+let times = 0
+
+cron.schedule('1-59/5 * * * * *', () => {
+    times++
+    console.log('Tick cada 5 segundos',times);
+});
+console.log('simple app ejecutado');
